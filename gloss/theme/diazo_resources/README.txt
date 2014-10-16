cd49cdc62b59a57d0dd616aaa7918558763e6945
David Bain <david@alteroo.com>
2014-10-16 06:30:23 -0500

    initial commit

---
+++ gloss/theme/diazo_resources/README.txt
@@ -0,0 +1,3 @@
+Place Diazo resources here, including rule and theme sources.
+
+It's common to develop these resources interactively via the Plone theming editor (Plone 4.3+), then export the resources and install them here.
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='gloss.theme',
+      version=version,
+      description="gloss.theme",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://svn.plone.org/svn/collective/',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['gloss'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'z3c.jbot',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
