c946ce1872b7e554213dee715ad48b73124f108f
David Bain <pigeonflight@gmail.com>
2015-09-30 02:14:04 +0000

    updated buildout.cfg and installer.sh

--- installer.sh
+++ installer.sh
@@ -1,5 +1,5 @@
 sudo apt-get install libxslt-dev libxml2-dev -y
-wget https://launchpad.net/plone/5.0/5.0/+download/Plone-5.0-UnifiedInstaller.tgz
+wget https://launchpad.net/plone/5.0/5.0/+download/Plone-5.0-UnifiedInstaller.tgz -O Plone-5.0-UnifiedInstaller.tgz
 tar xvfz Plone-5.0-UnifiedInstaller.tgz
 cd Plone-5.0-UnifiedInstaller
 tar xfj packages/buildout-cache.tar.bz2 
